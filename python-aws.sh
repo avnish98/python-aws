7c666f3b6d71017fc555ebc275a933554a474763
avnish98 <av_nish@outlook.com>
2019-02-03 20:41:34 +0530

    changed anaconda to miniconda

--- python-aws.sh
+++ python-aws.sh
@@ -5,7 +5,7 @@ apt-get install -y bzip2 gcc git htop screen vim wget
 apt-get upgrade -y bash
 apt-get clean
 
-# Downloading and installing anaconda
+# Downloading and installing Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O \
 Miniconda.sh
 bash Miniconda.sh -b
